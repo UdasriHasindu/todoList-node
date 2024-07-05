c8e6ad54e5a5937211d550e407c57e38ea518dda
u-sri2002 <usrimart@gmail.com>
2024-07-05 09:04:05 +0530

    build a structure to add new items to list

--- server.js
+++ server.js
@@ -3,18 +3,29 @@ const bodyParser = require('body-parser');
 const request = require('request');
 
 const app = express();
+
 app.use(express.static('views'));
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded( {extended: true}));
 
 
-const weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+const today = new Date();
+const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+const formattedDate = today.toLocaleDateString('en-US', options);
 
-const D = new Date();
-var today = D.getDay();
-
+let newItems = [];
 app.get('/', function(req, res){
-    res.render('list', {amazingDay: weekdays[today]});
-})
+    res.render('list', {amazingDay: formattedDate, userItems: newItems});
+});
+
+app.post('/', function(req, res){
+    const item = req.body.item;
+
+    newItems.push(item);
+    
+    res.redirect('/');
+
+});
 
 
 app.listen(5000, function(){
