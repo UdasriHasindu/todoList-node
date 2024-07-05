487cfc1f07004a9bddfc384ff3d472e722055dc3
u-sri2002 <usrimart@gmail.com>
2024-07-05 09:35:29 +0530

    add styles to publi folder

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const request = require('request');
 
 const app = express();
 
-app.use(express.static('views'));
+app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded( {extended: true}));
 
